67980c0603cd0400278ac1f2c678192409831010
Nihar Hegde <niharhegde163@gmail.com>
2023-12-06 11:45:21 +0530

    displaying the product on frontend

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { TQueryValidator } from "@/lib/validators/query-validator";
+import { Product } from "@/payload-types";
 import { trpc } from "@/trpc/client";
 import Link from "next/link";
 import React from "react";
+import ProductListing from "./ProductListing";
 
 interface ProductReelProps {
   title: string;
@@ -16,7 +18,7 @@ const FALLBACK_LIMIT = 4;
 const ProductReel = (props: ProductReelProps) => {
   const { title, subtitle, href, query } = props;
 
-  const { data } = trpc.getInfiniteProducts.useInfiniteQuery(
+  const { data: queryResults, isLoading } = trpc.getInfiniteProducts.useInfiniteQuery(
     {
       limit: query.limit ?? FALLBACK_LIMIT,
       query,
@@ -25,7 +27,16 @@ const ProductReel = (props: ProductReelProps) => {
       getNextPageParam: (lastPage) => lastPage.nextPage,
     }
   );
-  console.log(data);
+  const products = queryResults?.pages.flatMap((page) => page.items)
+
+  let map: (Product | null)[] = []
+  if (products && products.length) {
+    map = products
+  } else if (isLoading) {
+    map = new Array<null>(query.limit ?? FALLBACK_LIMIT).fill(null)
+  }
+
+
 
   return (
     <section className="py-12">
@@ -51,7 +62,13 @@ const ProductReel = (props: ProductReelProps) => {
       </div>
       <div className="relative">
         <div className="mt-6 flex items-center w-full">
-          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8"></div>
+          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
+            {
+              map.map((product, i) => (
+                <ProductListing product={product} index={i} />
+              ))
+            }
+          </div>
         </div>
       </div>
     </section>
