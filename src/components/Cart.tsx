1b1b5813d9e37f39e7039c02f4d20750fd3d7163
Nihar Hegde <niharhegde163@gmail.com>
2023-12-07 12:30:59 +0530

    adding to and removing from cart done, and fixed the image alignment issue in product details page

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -17,18 +17,23 @@ import Image from "next/image";
 import { useCart } from "@/hooks/use-cart";
 import { ScrollArea } from "./ui/scroll-area";
 import CartItem from "./CartItem";
-
-
-
+import { useEffect, useState } from "react";
 
 const Cart = () => {
-
-
   const { items } = useCart();
   const itemCount = items.length;
-  const cartTotal = items.reduce((total, { product }) => total + product.price, 0)
+
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
 
 
+  const cartTotal = items.reduce(
+    (total, { product }) => total + product.price,
+    0
+  );
 
   const fee = 1;
   return (
@@ -39,7 +44,7 @@ const Cart = () => {
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          {itemCount}
+          {isMounted ? itemCount : 0}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
@@ -50,11 +55,9 @@ const Cart = () => {
           <>
             <div className="flex w-full flex-col pr-6">
               <ScrollArea>
-                {
-                  items.map(({ product }) => (
-                    <CartItem product={product} key={product.id} />
-                  ))
-                }
+                {items.map(({ product }) => (
+                  <CartItem product={product} key={product.id} />
+                ))}
               </ScrollArea>
             </div>
             <div className="space-y-4 pr-6">
